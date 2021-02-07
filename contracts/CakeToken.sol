d6248677b679d9aa4b2ba6fd7196c873511d1e22
defifofum <defifofum0@gmail.com>
2021-02-07 12:42:14 -0500

    Update token details

--- contracts/CakeToken.sol
+++ contracts/CakeToken.sol
@@ -3,7 +3,7 @@ pragma solidity 0.6.12;
 import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/BEP20.sol";
 
 // CakeToken with Governance.
-contract CakeToken is BEP20('PancakeSwap Token', 'Cake') {
+contract CakeToken is BEP20('ApeSwap Banana Token', 'Banana') {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
