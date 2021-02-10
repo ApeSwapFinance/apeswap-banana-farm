fe6362fe683b5e4ca90ffa7ed04b35d515897f99
defifofum <defifofum0@gmail.com>
2021-02-10 00:10:46 -0500

    add details

--- contracts/Timelock.sol
+++ contracts/Timelock.sol
@@ -10,11 +10,12 @@
 
 /*
  * ApeSwapFinance 
- * App:      https://apeswap.finance
- * Medium:   https://medium/@ape_swap    
- * Twitter:  https://twitter.com/ape_swap 
- * Telegram: https://t.me/ape_swap
- * GitHub:   https://github.com/ApeSwapFinance
+ * App:             https://apeswap.finance
+ * Medium:          https://medium/@ape_swap    
+ * Twitter:         https://twitter.com/ape_swap 
+ * Telegram:        https://t.me/ape_swap
+ * Announcements:   https://t.me/ape_swap_news
+ * GitHub:          https://github.com/ApeSwapFinance
  */
 
 // XXX: pragma solidity ^0.5.16;
