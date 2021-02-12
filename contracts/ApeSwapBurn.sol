ae0da1aa218d7b8889fbf0fd323ca1903777cede
defifofum <defifofum0@gmail.com>
2021-02-11 22:26:51 -0500

    ApeSwapBurn contract, docs

---
+++ contracts/ApeSwapBurn.sol
@@ -0,0 +1,6 @@
+pragma solidity >=0.5.16;
+
+contract ApeSwapBurn {
+    string public name = "ApeSwapBurn";
+    string public description = "This contract is used as a record of tokens burned by ApeSwap Finance.";
+}
