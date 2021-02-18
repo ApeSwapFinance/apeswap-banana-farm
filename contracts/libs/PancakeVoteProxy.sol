9586f7f5634dca551076761c961e488558f2a6a9
masekztc <79254336+masekztc@users.noreply.github.com>
2021-02-18 11:58:12 +0100

    Update PancakeVoteProxy.sol

--- contracts/libs/PancakeVoteProxy.sol
+++ contracts/libs/PancakeVoteProxy.sol
@@ -97,7 +97,7 @@ interface IBEP20 {
 
 
 contract PancakeVoterProxy {
-    // BANANASPLIT
+    // BSTSPLIT
     address public constant votes = 0x009cF7bC57584b7998236eff51b98A168DceA9B0;
 
     function decimals() external pure returns (uint8) {
@@ -105,11 +105,11 @@ contract PancakeVoterProxy {
     }
 
     function name() external pure returns (string memory) {
-        return 'BANANASPLITVOTE';
+        return 'BSTSPLITVOTE';
     }
 
     function symbol() external pure returns (string memory) {
-        return 'BANANASPLIT';
+        return 'BSTSPLIT';
     }
 
     function totalSupply() external view returns (uint256) {
