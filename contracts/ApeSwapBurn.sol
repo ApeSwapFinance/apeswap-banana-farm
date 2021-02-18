5fd2d2f06306241051b7a6e6a889e8631480daae
masekztc <79254336+masekztc@users.noreply.github.com>
2021-02-18 11:28:27 +0100

    Update ApeSwapBurn.sol

--- contracts/ApeSwapBurn.sol
+++ contracts/ApeSwapBurn.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.5.16;
 
-contract ApeSwapBurn {
-    string public name = "ApeSwapBurn";
-    string public description = "This contract is used as a record of tokens burned by ApeSwap Finance.";
+contract CookiesSwapBurn {
+    string public name = "CookiesSwapBurn";
+    string public description = "This contract is used as a record of tokens burned by CookiesSwap Finance.";
 }
