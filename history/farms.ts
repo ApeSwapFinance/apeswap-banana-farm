1290c20e8045ec41141003b86e4d8a363de54cfe
Obie Dobo <obiedobo@gmail.com>
2021-08-27 15:28:25 -0500

    execute zig timelock

--- history/farms.ts
+++ history/farms.ts
@@ -35,13 +35,6 @@ export const queueFarmDetails: QueueFarmDetails[] = [
     //     status: 'queued'
     // }, 
     {
-        name: 'BNB/ZIG LP',
-        allocation: 75,
-        address: '0xeAB6A2186C506baf748DA8994f5Aa4acdE573142',
-        explorer: 'https://bscscan.com/address/0xeAB6A2186C506baf748DA8994f5Aa4acdE573142',
-        status: 'queued'
-    },
-    {
         name: 'BNB/LMT LP',
         allocation: 100,
         address: '0xbac4313d7f908c9a657b58b5b42f0f7713a3990b',
@@ -76,6 +69,14 @@ interface FarmDetails extends Omit<QueueFarmDetails, 'status' | 'nextAllocation.
 }
 
 export const farmDetails: FarmDetails[] = [
+    {
+        pid: 113,
+        name: 'BNB/ZIG LP',
+        allocation: 75,
+        address: '0xeAB6A2186C506baf748DA8994f5Aa4acdE573142',
+        explorer: 'https://bscscan.com/address/0xeAB6A2186C506baf748DA8994f5Aa4acdE573142',
+        status: 'active'
+    },
     {
         pid: 112,
         name: 'GNANA Farm',
