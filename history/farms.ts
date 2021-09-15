2e603b8b0936ed0c374f6efe14e503d687d017b4
Obie Dobo <obiedobo@gmail.com>
2021-09-15 15:21:27 -0500

    PHX reduction

--- history/farms.ts
+++ history/farms.ts
@@ -49,13 +49,6 @@ export const queueFarmDetails: QueueFarmDetails[] = [
         status: 'queued'
     },
     {
-        name: 'BNB/PHX LP',
-        allocation: 25,
-        address: '0xd6252ec3a5928d79f4211afdc0dd831a0fc97cdd',
-        explorer: 'https://bscscan.com/address/0xd6252ec3a5928d79f4211afdc0dd831a0fc97cdd',
-        status: 'queued'
-    },
-    {
         name: 'BNB/HUGO LP',
         allocation: 100,
         address: '0x6499B675EB745Fb2b63dc96f45A6Ea29F6172c46',
